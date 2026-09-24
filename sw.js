7a66e6afdff1703a51d7c0d200e9689cd3943f58
MarceloNeco <marceloneco@gmail.com>
2026-09-24 19:39:14 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Faz o site abrir sem internet e permite instalar como app.
    AO PUBLICAR UMA VERSÃO NOVA, troque o número abaixo (v1 -> v2).
    É o que avisa os celulares de que existe conteúdo novo. */
-var VERSAO = 'v3';
+var VERSAO = 'v4';
 var CACHE = 'moneytrio-' + VERSAO;
 
 self.addEventListener('install', function (e) {
